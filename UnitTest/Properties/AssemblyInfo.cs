bd891a6a5a84b2cfec6c35157e1bc68c89063280
Zhecha <j.prejs@yandex.com>
2018-12-18 17:11:23 +0300

    add test for container

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c62b918b-d383-42e7-ac3f-165db57ace46")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyInjectionContainer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public interface IClass1 { }
+        public abstract class Class1 : IClass1 { }
+        public interface IFoo { }
+        public class Class : Class1 { }
+        public class Foo : IFoo { }
+        public class Foo1 : IFoo { }
+        public class Bar : IFoo
+        {
+            public Class Class;
+            public Bar(Class1 class1)
+            {
+                Class = (Class)class1;
+            }
+        }
+        public class GenClass<T>
+        {
+            T field;
+        }
+        public interface IService<T> { }
+        public class ServiceImpl<T> : IService<T> { }
+
+        [TestMethod]
+        public void TestNoSingleton()
+        {
+            var dependencies = new DependenciesConfiguration();
+            dependencies.Register(typeof(IFoo), typeof(Foo), false);
+            var provider = new DependencyProvider(dependencies);
+            IFoo foo11 = provider.Resolve<IFoo>();
+            IFoo foo21 = provider.Resolve<IFoo>();
+            Assert.AreNotEqual(foo11,foo21);
+        }
+
+        [TestMethod]
+        public void TestSingleton()
+        {
+            var dependencies = new DependenciesConfiguration();
+            dependencies.Register(typeof(IFoo), typeof(Foo), true);
+            var provider = new DependencyProvider(dependencies);
+            IFoo foo1 = provider.Resolve<IFoo>();
+            IFoo foo2 = provider.Resolve<IFoo>();
+            Assert.AreEqual(foo1,foo2);
+        }
+
+        [TestMethod]
+        public void TestCountDependencies()
+        {
+            var dependencies = new DependenciesConfiguration();
+            dependencies.Register(typeof(IFoo), typeof(Foo), false);
+            dependencies.Register(typeof(IFoo), typeof(Foo1), false);
+            var provider = new DependencyProvider(dependencies);
+            var objects = provider.ResolveEnum<IFoo>();
+            Assert.AreEqual(objects.Count(),2);
+        }
+
+        [TestMethod]
+        public void TestGenClass()
+        {
+            var dependencies = new DependenciesConfiguration();
+            dependencies.Register(typeof(IFoo), typeof(Foo));
+            dependencies.Register(typeof(GenClass<IFoo>), typeof(GenClass<IFoo>));
+            var provider = new DependencyProvider(dependencies);
+            GenClass<IFoo> obj = provider.Resolve<GenClass<IFoo>>();
+            Assert.IsNotNull(obj);
+        }
+    }
+}
