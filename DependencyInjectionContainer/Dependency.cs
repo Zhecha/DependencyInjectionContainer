09dc3d782dc8294f17953d824c3477756ad06003
Zhecha <j.prejs@yandex.com>
2018-12-18 07:37:16 +0300

    add dependency class

---
+++ DependencyInjectionContainer/Dependency.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer
+{
+    public class Dependency
+    {
+        public Type _interface;
+        public Type _realiz;
+        public bool _singleton;
+        public object Getinstance { get; set; }
+
+        public Dependency(Type Interface, Type Realiz, bool Singleton = false)
+        {
+            bool check = (!Realiz.IsInterface && !Realiz.IsAbstract && (Interface.IsAssignableFrom(Realiz) || Realiz.IsGenericTypeDefinition)) ? true : false;
+            if (check)
+            {
+                _interface = Interface;
+                _realiz = Realiz;
+                _singleton = Singleton;
+                Getinstance = null;
+            }
+        }
+    }
+}
